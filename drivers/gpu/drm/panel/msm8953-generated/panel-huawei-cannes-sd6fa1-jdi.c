4f24d5163a6aa5a900b38d7efa8f0bae6cd453bd
lmdpdg <lmdpdg@localhost>
2025-01-18 21:34:01 +0100

    squash! MSM8953: drm/panel: Generate using linux-mdss-dsi-panel-driver-generator
    X-Code-Generator: msm8953-mainline/linux-panel-drivers@92be81b (dirty)
    Signed-off-by: lmdpdg <lmdpdg@localhost>

--- drivers/gpu/drm/panel/msm8953-generated/panel-huawei-cannes-sd6fa1-jdi.c
+++ drivers/gpu/drm/panel/msm8953-generated/panel-huawei-cannes-sd6fa1-jdi.c
@@ -14,24 +14,24 @@
 #include <drm/drm_panel.h>
 #include <drm/drm_probe_helper.h>
 
-struct jdi_r63452_5p5 {
+struct jdi_sd6fa1_5p0 {
 	struct drm_panel panel;
 	struct mipi_dsi_device *dsi;
 	struct regulator_bulk_data *supplies;
 	struct gpio_desc *reset_gpio;
 };
 
-static const struct regulator_bulk_data jdi_r63452_5p5_supplies[] = {
+static const struct regulator_bulk_data jdi_sd6fa1_5p0_supplies[] = {
 	{ .supply = "vsn" },
 	{ .supply = "vsp" },
 };
 
-static inline struct jdi_r63452_5p5 *to_jdi_r63452_5p5(struct drm_panel *panel)
+static inline struct jdi_sd6fa1_5p0 *to_jdi_sd6fa1_5p0(struct drm_panel *panel)
 {
-	return container_of(panel, struct jdi_r63452_5p5, panel);
+	return container_of(panel, struct jdi_sd6fa1_5p0, panel);
 }
 
-static void jdi_r63452_5p5_reset(struct jdi_r63452_5p5 *ctx)
+static void jdi_sd6fa1_5p0_reset(struct jdi_sd6fa1_5p0 *ctx)
 {
 	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
 	usleep_range(10000, 11000);
@@ -41,113 +41,116 @@ static void jdi_r63452_5p5_reset(struct jdi_r63452_5p5 *ctx)
 	usleep_range(10000, 11000);
 }
 
-static int jdi_r63452_5p5_on(struct jdi_r63452_5p5 *ctx)
+static int jdi_sd6fa1_5p0_on(struct jdi_sd6fa1_5p0 *ctx)
 {
 	struct mipi_dsi_multi_context dsi_ctx = { .dsi = ctx->dsi };
 
-	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0xb0, 0x00);
-	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0xca,
-					 0x1d, 0xfc, 0xfc, 0xfc, 0x00, 0xfa,
-					 0x01, 0x9c, 0x00, 0xf5, 0xe0, 0x05,
-					 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-					 0x00, 0x00, 0x00, 0x00, 0x9f, 0x6e,
-					 0x61, 0x9d, 0x69, 0x5b, 0x2d, 0x00,
-					 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-					 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-					 0x00);
+	ctx->dsi->mode_flags |= MIPI_DSI_MODE_LPM;
+
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf0, 0x5a, 0x5a);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf1, 0x5a, 0x5a);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xfc, 0x5a, 0x5a);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xe0, 0x01, 0x00, 0x00);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xb0, 0x05);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xe2, 0xf5);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf0, 0xa5, 0xa5);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf1, 0xa5, 0xa5);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xfc, 0xa5, 0xa5);
 	mipi_dsi_dcs_set_tear_on_multi(&dsi_ctx, MIPI_DSI_DCS_TEAR_MODE_VBLANK);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0x11, 0x00);
 	mipi_dsi_msleep(&dsi_ctx, 120);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0x29, 0x00);
-	mipi_dsi_usleep_range(&dsi_ctx, 10000, 11000);
+	mipi_dsi_msleep(&dsi_ctx, 20);
 
 	return dsi_ctx.accum_err;
 }
 
-static int jdi_r63452_5p5_off(struct jdi_r63452_5p5 *ctx)
+static int jdi_sd6fa1_5p0_off(struct jdi_sd6fa1_5p0 *ctx)
 {
 	struct mipi_dsi_multi_context dsi_ctx = { .dsi = ctx->dsi };
 
+	ctx->dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
+
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0x28, 0x00);
-	mipi_dsi_msleep(&dsi_ctx, 20);
+	mipi_dsi_msleep(&dsi_ctx, 32);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0x10, 0x00);
 	mipi_dsi_msleep(&dsi_ctx, 120);
 
 	return dsi_ctx.accum_err;
 }
 
-static int jdi_r63452_5p5_prepare(struct drm_panel *panel)
+static int jdi_sd6fa1_5p0_prepare(struct drm_panel *panel)
 {
-	struct jdi_r63452_5p5 *ctx = to_jdi_r63452_5p5(panel);
+	struct jdi_sd6fa1_5p0 *ctx = to_jdi_sd6fa1_5p0(panel);
 	struct device *dev = &ctx->dsi->dev;
 	int ret;
 
-	ret = regulator_bulk_enable(ARRAY_SIZE(jdi_r63452_5p5_supplies), ctx->supplies);
+	ret = regulator_bulk_enable(ARRAY_SIZE(jdi_sd6fa1_5p0_supplies), ctx->supplies);
 	if (ret < 0) {
 		dev_err(dev, "Failed to enable regulators: %d\n", ret);
 		return ret;
 	}
 
-	jdi_r63452_5p5_reset(ctx);
+	jdi_sd6fa1_5p0_reset(ctx);
 
-	ret = jdi_r63452_5p5_on(ctx);
+	ret = jdi_sd6fa1_5p0_on(ctx);
 	if (ret < 0) {
 		dev_err(dev, "Failed to initialize panel: %d\n", ret);
 		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-		regulator_bulk_disable(ARRAY_SIZE(jdi_r63452_5p5_supplies), ctx->supplies);
+		regulator_bulk_disable(ARRAY_SIZE(jdi_sd6fa1_5p0_supplies), ctx->supplies);
 		return ret;
 	}
 
 	return 0;
 }
 
-static int jdi_r63452_5p5_unprepare(struct drm_panel *panel)
+static int jdi_sd6fa1_5p0_unprepare(struct drm_panel *panel)
 {
-	struct jdi_r63452_5p5 *ctx = to_jdi_r63452_5p5(panel);
+	struct jdi_sd6fa1_5p0 *ctx = to_jdi_sd6fa1_5p0(panel);
 	struct device *dev = &ctx->dsi->dev;
 	int ret;
 
-	ret = jdi_r63452_5p5_off(ctx);
+	ret = jdi_sd6fa1_5p0_off(ctx);
 	if (ret < 0)
 		dev_err(dev, "Failed to un-initialize panel: %d\n", ret);
 
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-	regulator_bulk_disable(ARRAY_SIZE(jdi_r63452_5p5_supplies), ctx->supplies);
+	regulator_bulk_disable(ARRAY_SIZE(jdi_sd6fa1_5p0_supplies), ctx->supplies);
 
 	return 0;
 }
 
-static const struct drm_display_mode jdi_r63452_5p5_mode = {
-	.clock = (1080 + 45 + 8 + 45) * (1920 + 16 + 4 + 16) * 60 / 1000,
+static const struct drm_display_mode jdi_sd6fa1_5p0_mode = {
+	.clock = (1080 + 116 + 8 + 48) * (1920 + 4 + 4 + 3) * 60 / 1000,
 	.hdisplay = 1080,
-	.hsync_start = 1080 + 45,
-	.hsync_end = 1080 + 45 + 8,
-	.htotal = 1080 + 45 + 8 + 45,
+	.hsync_start = 1080 + 116,
+	.hsync_end = 1080 + 116 + 8,
+	.htotal = 1080 + 116 + 8 + 48,
 	.vdisplay = 1920,
-	.vsync_start = 1920 + 16,
-	.vsync_end = 1920 + 16 + 4,
-	.vtotal = 1920 + 16 + 4 + 16,
+	.vsync_start = 1920 + 4,
+	.vsync_end = 1920 + 4 + 4,
+	.vtotal = 1920 + 4 + 4 + 3,
 	.width_mm = 68,
 	.height_mm = 121,
 	.type = DRM_MODE_TYPE_DRIVER,
 };
 
-static int jdi_r63452_5p5_get_modes(struct drm_panel *panel,
+static int jdi_sd6fa1_5p0_get_modes(struct drm_panel *panel,
 				    struct drm_connector *connector)
 {
-	return drm_connector_helper_get_modes_fixed(connector, &jdi_r63452_5p5_mode);
+	return drm_connector_helper_get_modes_fixed(connector, &jdi_sd6fa1_5p0_mode);
 }
 
-static const struct drm_panel_funcs jdi_r63452_5p5_panel_funcs = {
-	.prepare = jdi_r63452_5p5_prepare,
-	.unprepare = jdi_r63452_5p5_unprepare,
-	.get_modes = jdi_r63452_5p5_get_modes,
+static const struct drm_panel_funcs jdi_sd6fa1_5p0_panel_funcs = {
+	.prepare = jdi_sd6fa1_5p0_prepare,
+	.unprepare = jdi_sd6fa1_5p0_unprepare,
+	.get_modes = jdi_sd6fa1_5p0_get_modes,
 };
 
-static int jdi_r63452_5p5_probe(struct mipi_dsi_device *dsi)
+static int jdi_sd6fa1_5p0_probe(struct mipi_dsi_device *dsi)
 {
 	struct device *dev = &dsi->dev;
-	struct jdi_r63452_5p5 *ctx;
+	struct jdi_sd6fa1_5p0 *ctx;
 	int ret;
 
 	ctx = devm_kzalloc(dev, sizeof(*ctx), GFP_KERNEL);
@@ -155,8 +158,8 @@ static int jdi_r63452_5p5_probe(struct mipi_dsi_device *dsi)
 		return -ENOMEM;
 
 	ret = devm_regulator_bulk_get_const(dev,
-					    ARRAY_SIZE(jdi_r63452_5p5_supplies),
-					    jdi_r63452_5p5_supplies,
+					    ARRAY_SIZE(jdi_sd6fa1_5p0_supplies),
+					    jdi_sd6fa1_5p0_supplies,
 					    &ctx->supplies);
 	if (ret < 0)
 		return ret;
@@ -171,10 +174,11 @@ static int jdi_r63452_5p5_probe(struct mipi_dsi_device *dsi)
 
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
-	dsi->mode_flags = MIPI_DSI_MODE_VIDEO_BURST | MIPI_DSI_MODE_VIDEO_HSE |
-			  MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM;
+	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
+			  MIPI_DSI_MODE_VIDEO_HSE |
+			  MIPI_DSI_CLOCK_NON_CONTINUOUS;
 
-	drm_panel_init(&ctx->panel, dev, &jdi_r63452_5p5_panel_funcs,
+	drm_panel_init(&ctx->panel, dev, &jdi_sd6fa1_5p0_panel_funcs,
 		       DRM_MODE_CONNECTOR_DSI);
 
 	ret = drm_panel_of_backlight(&ctx->panel);
@@ -192,9 +196,9 @@ static int jdi_r63452_5p5_probe(struct mipi_dsi_device *dsi)
 	return 0;
 }
 
-static void jdi_r63452_5p5_remove(struct mipi_dsi_device *dsi)
+static void jdi_sd6fa1_5p0_remove(struct mipi_dsi_device *dsi)
 {
-	struct jdi_r63452_5p5 *ctx = mipi_dsi_get_drvdata(dsi);
+	struct jdi_sd6fa1_5p0 *ctx = mipi_dsi_get_drvdata(dsi);
 	int ret;
 
 	ret = mipi_dsi_detach(dsi);
@@ -204,22 +208,22 @@ static void jdi_r63452_5p5_remove(struct mipi_dsi_device *dsi)
 	drm_panel_remove(&ctx->panel);
 }
 
-static const struct of_device_id jdi_r63452_5p5_of_match[] = {
-	{ .compatible = "huawei,milan-jdi-r63452" }, // FIXME
+static const struct of_device_id jdi_sd6fa1_5p0_of_match[] = {
+	{ .compatible = "huawei,cannes-sd6fa1-jdi" }, // FIXME
 	{ /* sentinel */ }
 };
-MODULE_DEVICE_TABLE(of, jdi_r63452_5p5_of_match);
+MODULE_DEVICE_TABLE(of, jdi_sd6fa1_5p0_of_match);
 
-static struct mipi_dsi_driver jdi_r63452_5p5_driver = {
-	.probe = jdi_r63452_5p5_probe,
-	.remove = jdi_r63452_5p5_remove,
+static struct mipi_dsi_driver jdi_sd6fa1_5p0_driver = {
+	.probe = jdi_sd6fa1_5p0_probe,
+	.remove = jdi_sd6fa1_5p0_remove,
 	.driver = {
-		.name = "panel-jdi-r63452-5p5",
-		.of_match_table = jdi_r63452_5p5_of_match,
+		.name = "panel-jdi-sd6fa1-5p0",
+		.of_match_table = jdi_sd6fa1_5p0_of_match,
 	},
 };
-module_mipi_dsi_driver(jdi_r63452_5p5_driver);
+module_mipi_dsi_driver(jdi_sd6fa1_5p0_driver);
 
 MODULE_AUTHOR("linux-mdss-dsi-panel-driver-generator <fix@me>"); // FIXME
-MODULE_DESCRIPTION("DRM driver for JDI_R63452_5P5_1080P_CMD");
+MODULE_DESCRIPTION("DRM driver for JDI_SD6FA1_5P0_1080P_VIDEO");
 MODULE_LICENSE("GPL");
--- drivers/gpu/drm/panel/msm8953-generated/panel-huawei-cannes-td4300-boe.c
+++ drivers/gpu/drm/panel/msm8953-generated/panel-huawei-cannes-td4300-boe.c
@@ -9,142 +9,145 @@
 #include <linux/module.h>
 #include <linux/regulator/consumer.h>
 
+#include <video/mipi_display.h>
+
 #include <drm/drm_mipi_dsi.h>
 #include <drm/drm_modes.h>
 #include <drm/drm_panel.h>
 #include <drm/drm_probe_helper.h>
 
-struct ili9885_boe {
+struct boe_td4300_5p0 {
 	struct drm_panel panel;
 	struct mipi_dsi_device *dsi;
 	struct regulator_bulk_data *supplies;
 	struct gpio_desc *reset_gpio;
 };
 
-static const struct regulator_bulk_data ili9885_boe_supplies[] = {
+static const struct regulator_bulk_data boe_td4300_5p0_supplies[] = {
 	{ .supply = "vsn" },
 	{ .supply = "vsp" },
 };
 
-static inline struct ili9885_boe *to_ili9885_boe(struct drm_panel *panel)
+static inline struct boe_td4300_5p0 *to_boe_td4300_5p0(struct drm_panel *panel)
 {
-	return container_of(panel, struct ili9885_boe, panel);
+	return container_of(panel, struct boe_td4300_5p0, panel);
 }
 
-static void ili9885_boe_reset(struct ili9885_boe *ctx)
+static void boe_td4300_5p0_reset(struct boe_td4300_5p0 *ctx)
 {
 	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
 	usleep_range(10000, 11000);
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-	usleep_range(10000, 11000);
+	usleep_range(5000, 6000);
 	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
 	usleep_range(10000, 11000);
 }
 
-static int ili9885_boe_on(struct ili9885_boe *ctx)
+static int boe_td4300_5p0_on(struct boe_td4300_5p0 *ctx)
 {
 	struct mipi_dsi_multi_context dsi_ctx = { .dsi = ctx->dsi };
 
 	ctx->dsi->mode_flags |= MIPI_DSI_MODE_LPM;
 
-	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0x35, 0x00);
-	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0x51, 0x0f, 0xff);
-	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0x53, 0x2c);
-	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0x55, 0x00);
-	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0x11, 0x00);
+	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0xb0, 0x04);
+	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0xf3, 0x02, 0x00, 0xb4);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, MIPI_DCS_WRITE_CONTROL_DISPLAY,
+				     0x0c);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, MIPI_DCS_WRITE_POWER_SAVE, 0x00);
+	mipi_dsi_dcs_exit_sleep_mode_multi(&dsi_ctx);
 	mipi_dsi_msleep(&dsi_ctx, 120);
-	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0x29, 0x00);
-	mipi_dsi_usleep_range(&dsi_ctx, 5000, 6000);
+	mipi_dsi_dcs_set_display_on_multi(&dsi_ctx);
+	mipi_dsi_msleep(&dsi_ctx, 20);
 
 	return dsi_ctx.accum_err;
 }
 
-static int ili9885_boe_off(struct ili9885_boe *ctx)
+static int boe_td4300_5p0_off(struct boe_td4300_5p0 *ctx)
 {
 	struct mipi_dsi_multi_context dsi_ctx = { .dsi = ctx->dsi };
 
 	ctx->dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
 
 	mipi_dsi_dcs_set_display_off_multi(&dsi_ctx);
-	mipi_dsi_msleep(&dsi_ctx, 20);
+	mipi_dsi_msleep(&dsi_ctx, 32);
 	mipi_dsi_dcs_enter_sleep_mode_multi(&dsi_ctx);
 	mipi_dsi_msleep(&dsi_ctx, 120);
 
 	return dsi_ctx.accum_err;
 }
 
-static int ili9885_boe_prepare(struct drm_panel *panel)
+static int boe_td4300_5p0_prepare(struct drm_panel *panel)
 {
-	struct ili9885_boe *ctx = to_ili9885_boe(panel);
+	struct boe_td4300_5p0 *ctx = to_boe_td4300_5p0(panel);
 	struct device *dev = &ctx->dsi->dev;
 	int ret;
 
-	ret = regulator_bulk_enable(ARRAY_SIZE(ili9885_boe_supplies), ctx->supplies);
+	ret = regulator_bulk_enable(ARRAY_SIZE(boe_td4300_5p0_supplies), ctx->supplies);
 	if (ret < 0) {
 		dev_err(dev, "Failed to enable regulators: %d\n", ret);
 		return ret;
 	}
 
-	ili9885_boe_reset(ctx);
+	boe_td4300_5p0_reset(ctx);
 
-	ret = ili9885_boe_on(ctx);
+	ret = boe_td4300_5p0_on(ctx);
 	if (ret < 0) {
 		dev_err(dev, "Failed to initialize panel: %d\n", ret);
 		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-		regulator_bulk_disable(ARRAY_SIZE(ili9885_boe_supplies), ctx->supplies);
+		regulator_bulk_disable(ARRAY_SIZE(boe_td4300_5p0_supplies), ctx->supplies);
 		return ret;
 	}
 
 	return 0;
 }
 
-static int ili9885_boe_unprepare(struct drm_panel *panel)
+static int boe_td4300_5p0_unprepare(struct drm_panel *panel)
 {
-	struct ili9885_boe *ctx = to_ili9885_boe(panel);
+	struct boe_td4300_5p0 *ctx = to_boe_td4300_5p0(panel);
 	struct device *dev = &ctx->dsi->dev;
 	int ret;
 
-	ret = ili9885_boe_off(ctx);
+	ret = boe_td4300_5p0_off(ctx);
 	if (ret < 0)
 		dev_err(dev, "Failed to un-initialize panel: %d\n", ret);
 
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-	regulator_bulk_disable(ARRAY_SIZE(ili9885_boe_supplies), ctx->supplies);
+	regulator_bulk_disable(ARRAY_SIZE(boe_td4300_5p0_supplies), ctx->supplies);
 
 	return 0;
 }
 
-static const struct drm_display_mode ili9885_boe_mode = {
-	.clock = (1080 + 100 + 16 + 64) * (1920 + 44 + 2 + 12) * 60 / 1000,
+static const struct drm_display_mode boe_td4300_5p0_mode = {
+	.clock = (1080 + 107 + 2 + 50) * (1920 + 10 + 2 + 6) * 60 / 1000,
 	.hdisplay = 1080,
-	.hsync_start = 1080 + 100,
-	.hsync_end = 1080 + 100 + 16,
-	.htotal = 1080 + 100 + 16 + 64,
+	.hsync_start = 1080 + 107,
+	.hsync_end = 1080 + 107 + 2,
+	.htotal = 1080 + 107 + 2 + 50,
 	.vdisplay = 1920,
-	.vsync_start = 1920 + 44,
-	.vsync_end = 1920 + 44 + 2,
-	.vtotal = 1920 + 44 + 2 + 12,
-	.width_mm = 69,
-	.height_mm = 122,
+	.vsync_start = 1920 + 10,
+	.vsync_end = 1920 + 10 + 2,
+	.vtotal = 1920 + 10 + 2 + 6,
+	.width_mm = 62,
+	.height_mm = 110,
 	.type = DRM_MODE_TYPE_DRIVER,
 };
 
-static int ili9885_boe_get_modes(struct drm_panel *panel,
-				 struct drm_connector *connector)
+static int boe_td4300_5p0_get_modes(struct drm_panel *panel,
+				    struct drm_connector *connector)
 {
-	return drm_connector_helper_get_modes_fixed(connector, &ili9885_boe_mode);
+	return drm_connector_helper_get_modes_fixed(connector, &boe_td4300_5p0_mode);
 }
 
-static const struct drm_panel_funcs ili9885_boe_panel_funcs = {
-	.prepare = ili9885_boe_prepare,
-	.unprepare = ili9885_boe_unprepare,
-	.get_modes = ili9885_boe_get_modes,
+static const struct drm_panel_funcs boe_td4300_5p0_panel_funcs = {
+	.prepare = boe_td4300_5p0_prepare,
+	.unprepare = boe_td4300_5p0_unprepare,
+	.get_modes = boe_td4300_5p0_get_modes,
 };
 
-static int ili9885_boe_probe(struct mipi_dsi_device *dsi)
+static int boe_td4300_5p0_probe(struct mipi_dsi_device *dsi)
 {
 	struct device *dev = &dsi->dev;
-	struct ili9885_boe *ctx;
+	struct boe_td4300_5p0 *ctx;
 	int ret;
 
 	ctx = devm_kzalloc(dev, sizeof(*ctx), GFP_KERNEL);
@@ -152,8 +155,8 @@ static int ili9885_boe_probe(struct mipi_dsi_device *dsi)
 		return -ENOMEM;
 
 	ret = devm_regulator_bulk_get_const(dev,
-					    ARRAY_SIZE(ili9885_boe_supplies),
-					    ili9885_boe_supplies,
+					    ARRAY_SIZE(boe_td4300_5p0_supplies),
+					    boe_td4300_5p0_supplies,
 					    &ctx->supplies);
 	if (ret < 0)
 		return ret;
@@ -169,10 +172,10 @@ static int ili9885_boe_probe(struct mipi_dsi_device *dsi)
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
-			  MIPI_DSI_MODE_VIDEO_HSE | MIPI_DSI_MODE_NO_EOT_PACKET |
+			  MIPI_DSI_MODE_VIDEO_HSE |
 			  MIPI_DSI_CLOCK_NON_CONTINUOUS;
 
-	drm_panel_init(&ctx->panel, dev, &ili9885_boe_panel_funcs,
+	drm_panel_init(&ctx->panel, dev, &boe_td4300_5p0_panel_funcs,
 		       DRM_MODE_CONNECTOR_DSI);
 
 	ret = drm_panel_of_backlight(&ctx->panel);
@@ -190,9 +193,9 @@ static int ili9885_boe_probe(struct mipi_dsi_device *dsi)
 	return 0;
 }
 
-static void ili9885_boe_remove(struct mipi_dsi_device *dsi)
+static void boe_td4300_5p0_remove(struct mipi_dsi_device *dsi)
 {
-	struct ili9885_boe *ctx = mipi_dsi_get_drvdata(dsi);
+	struct boe_td4300_5p0 *ctx = mipi_dsi_get_drvdata(dsi);
 	int ret;
 
 	ret = mipi_dsi_detach(dsi);
@@ -202,22 +205,22 @@ static void ili9885_boe_remove(struct mipi_dsi_device *dsi)
 	drm_panel_remove(&ctx->panel);
 }
 
-static const struct of_device_id ili9885_boe_of_match[] = {
-	{ .compatible = "xiaomi,boe-ili9885" }, // FIXME
+static const struct of_device_id boe_td4300_5p0_of_match[] = {
+	{ .compatible = "huawei,cannes-td4300-boe" }, // FIXME
 	{ /* sentinel */ }
 };
-MODULE_DEVICE_TABLE(of, ili9885_boe_of_match);
+MODULE_DEVICE_TABLE(of, boe_td4300_5p0_of_match);
 
-static struct mipi_dsi_driver ili9885_boe_driver = {
-	.probe = ili9885_boe_probe,
-	.remove = ili9885_boe_remove,
+static struct mipi_dsi_driver boe_td4300_5p0_driver = {
+	.probe = boe_td4300_5p0_probe,
+	.remove = boe_td4300_5p0_remove,
 	.driver = {
-		.name = "panel-ili9885-boe",
-		.of_match_table = ili9885_boe_of_match,
+		.name = "panel-boe-td4300-5p0",
+		.of_match_table = boe_td4300_5p0_of_match,
 	},
 };
-module_mipi_dsi_driver(ili9885_boe_driver);
+module_mipi_dsi_driver(boe_td4300_5p0_driver);
 
 MODULE_AUTHOR("linux-mdss-dsi-panel-driver-generator <fix@me>"); // FIXME
-MODULE_DESCRIPTION("DRM driver for ili9885 boe fhd video mode dsi panel");
+MODULE_DESCRIPTION("DRM driver for BOE_TD4300_1080P_VIDEO");
 MODULE_LICENSE("GPL");
